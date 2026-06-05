a1821ed4d7e8e57f9f3b5af82a7a950ee24eabff
kimyoungwook730 <wook5272@naver.com>
2026-06-05 12:40:16 +0900

    Hello_commit

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,8 +1,8 @@
 export default function Hello () {
     return(
         <div>
-        <h1>Hello, GitHub Page !</h1>
-        <h2>안녕하세요.</h2>
+            <h1>Hello, GitHub Page !</h1>
+            <h2>안녕하세요.</h2>
         </div>
     )
 }
